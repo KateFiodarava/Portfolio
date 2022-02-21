35d4db6d6bd45cc9077ef55daf8ae20d1992d5bc
KateFiodarava <apanasevech@mail.ru>
2022-02-21 23:37:18 +0300

    add style

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -2,23 +2,32 @@ import style from './Projects.module.css'
 import styleContainer from '../common/styles/Container.module.css'
 import {Project} from "./project/Project";
 import {Title} from "../common/components/title/Title";
+import todoImage from "./../assets/image/Todolist.jpg"
+import socialImage from "./../assets/image/SocialNetwork.jpg"
 
 
 export const Projects = () => {
+    const social={
+        backgroundImage:`url(${socialImage})`,
+    }
+    const todolist={
+        backgroundImage:`url(${todoImage})`,
+    }
     return (
         <div className={style.projectsBlock}>
             <div
                 className={`${styleContainer.container} ${style.projectsContainer}`}>
            <Title title={'Projects'}/>
                 <div className={style.projects}>
-                    <Project title={'Social Network'}
+                    <Project  style={social}
+                              title={'Social Network'}
                              description={'My first project'}
-                             img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCpSrZAVPLNvI27A1G3Jdu_sdxWiF0vM6JPA&usqp=CAU'}
                              link={'https://KateFiodarava.github.io/SocialNetwork'}
                     />
-                    <Project title={'Todolist'}
+                    <Project
+                        style={todolist}
+                        title={'Todolist'}
                              description={'My second project'}
-                             img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkLw7bzZ941FkamKLIQGeyUkTTytZL-BGYPQ&usqp=CAU'}
                              link={'https://www.google.com/search?q=%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4%D1%87%D0%B8%D0%BA&spell=1&sa=X&ved=2ahUKEwjAhrji9eDyAhXGhf0HHQFZBocQBSgAegQIARA0&biw=1366&bih=657'}
                     />
                 </div>
--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -4,13 +4,17 @@ import style from './Project.module.css'
 export const Project = (props) => {
     return (
         <div className={style.project}>
-            <div className={style.imageContainer}>
-           <a href={props.link}> <img className={style.icon} src={props.img} />show </a>
+            <div className={style.imageContainer} style={props.style}>
+           <a href={props.link}
+           className={style.projectButton}> Open project </a>
             </div>
-            <h3 className={style.title}>{props.title}</h3>
-            <span className={style.description}>
+            <div className={style.projectInfo}>
+                <h3 className={style.projectTitle}>{props.title}</h3>
+                <span className={style.description}>
                 {props.description}
             </span>
+            </div>
+
 
         </div>
     )
