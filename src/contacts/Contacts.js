baf75f65606d354248f79cf45a698aa59ce37009
KateFiodarava <apanasevech@mail.ru>
2022-03-14 22:38:30 +0300

    add emailjs

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,22 +1,49 @@
 import styles from './Contacts.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
+import emailjs from '@emailjs/browser';
+import React, {FormEvent, useState} from "react"
 
 export const Contacts = () => {
+
+    const [emailValue, setEmailValue] = useState('')
+    const addEmailValue = (e: FormEvent<HTMLInputElement>) => {
+        setEmailValue(e.currentTarget.value)
+    }
+
+    const sendEmail = (e: any) => {
+        setEmailValue('')
+        e.preventDefault();
+
+        emailjs.sendForm('service_lztcn3q', 'template_b4xmnzj', e.target,
+            '6Z9LAoKT1rQirMWDw')
+            .then((result) => {
+
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+
+        setTimeout(() => {
+            alert('Message sent')
+        }, 1000)
+    }
+
+
     return (
         <section className={styles.contactsBlock} id={'contactsId'}>
             <div
                 className={`${styleContainer.container} ${styles.contactsContainer}`}>
                 <h2 className={styles.title}>Contacts</h2>
-                <form className={styles.formContainer}>
+                <form method={'POST'} onSubmit={sendEmail} className={styles.formContainer}>
                     <ul>
-                        <li><input placeholder={'Name'} type={'text'}/></li>
-                        <li><input  placeholder={'E-mail'} type={'text'}/></li>
-                        <li><textarea placeholder={'Your message'} /></li>
-                        <li> <button type={'submit'} className={styles.submitBtn}>Send</button></li>
+                        <li><input placeholder={'Name'} type={'text'} name={'name'}/></li>
+                        <li><input value={emailValue} onInput={addEmailValue} placeholder={'E-mail'} type={'text'}  name={'email'}/></li>
+                        <li><textarea placeholder={'Your message'} name={'message'}/></li>
+                        <li> <button type={'submit'} className={styles.submitBtn}   disabled={!emailValue}>Send</button></li>
                     </ul>
                 </form>
             </div>
         </section>
     )
 }
-
